172285c28520dfcac357361978e39f7c37ca13b2
Chr L <titian@boxbox.org>
2012-08-25 03:58:08 +0200

    i wanna c u move on da floor
    By using triangular functions, the player can be moved in the horizontal
    plane according to the current orientation.

--- Game/Entities/ActiveEntity.cs
+++ Game/Entities/ActiveEntity.cs
@@ -6,6 +6,8 @@ namespace DynaStudios.LD24.Game.Entities
 {
     public abstract class ActiveEntity : Entity
     {
+		protected float moving_speed_s = 10.0f;
+		protected float turning_speed_s = 100.0f;
         public virtual void Update(GameTime gameTime)
         {
             Position.UpdateVisual(gameTime.TotalGameTime);
--- Game/Entities/Player.cs
+++ Game/Entities/Player.cs
@@ -37,27 +37,74 @@ namespace DynaStudios.LD24.Game.Entities
             UpdateInput(gameTime);
         }
 
-        private void UpdateInput(GameTime gameTime) {
-            var keyboardState = screen.input.KeyboardState;
-            var position = Position.Real;
+		// for better overview define...
+		private float sin (double ang) { return (float) Math.Sin (ang/180.0*Math.PI); }
+		private float cos (double ang) { return (float) Math.Cos (ang/180.0*Math.PI); }
+		private float sqrt2 = (float) Math.Sqrt(2.0) * 0.5f;
 
-            if (keyboardState.IsKeyDown(Keys.W))
-            {
-                position.Z += 1.0f;
-            }
-            else if (keyboardState.IsKeyDown(Keys.S))
-            {
-                position.Z -= 1.0f;
-            }
-            if (keyboardState.IsKeyDown(Keys.A))
-            {
-                position.X -= 1.0f;
-            }
-            else
-            {
-                position.X += 1.0f;
-            }
+        private void UpdateInput (GameTime gameTime)
+		{
+			var keyboardState = screen.input.KeyboardState;
+			var position = Position.Real;
+			var direction = Direction;
+			float distance= moving_speed_s / 60.0f; // the distance this object can move during this update
+			float angle   = turning_speed_s / 60.0f; // the angle by which this object can turn during this update
 
+			if (keyboardState.IsKeyDown (Keys.Up)) {
+				direction.X -= angle;
+			}
+			if (keyboardState.IsKeyDown (Keys.Down)) {
+				direction.X += angle;
+			}
+			if (keyboardState.IsKeyDown (Keys.Left)) {
+				direction.Y -= angle;
+			}
+			if (keyboardState.IsKeyDown (Keys.Right)) {
+				direction.Y += angle;
+			}
+			Direction=direction;
+
+			//again, for easier understanding of the code...
+			bool keyLeft =  keyboardState.IsKeyDown (Keys.A);
+			bool keyRight = keyboardState.IsKeyDown (Keys.D);
+			bool keyFore =  keyboardState.IsKeyDown (Keys.W);
+			bool keyBack =  keyboardState.IsKeyDown (Keys.S);
+
+			if (keyLeft) {
+				//            use triangular functions
+				//            so we move around the floor
+				//            according to "Direction"
+				//                                            multiplying by "sqrt2" asserts that
+				//                                            this object can not move faster than
+				//                                            "distance" by using two non-linear
+				//                                            directions (e.g. forewards and
+				//                                            sidewards) at the same time
+				position.X -= cos (Direction.Y) * distance * (keyFore || keyBack ? sqrt2 : 1.0f);
+				position.Z -= sin (Direction.Y) * distance * (keyFore || keyBack ? sqrt2 : 1.0f);
+			}
+			if (keyRight) {
+				position.X += cos (Direction.Y) * distance * (keyFore || keyBack ? sqrt2 : 1.0f);
+				position.Z += sin (Direction.Y) * distance * (keyFore || keyBack ? sqrt2 : 1.0f);
+			}
+			if (keyFore) {
+				position.Z -= cos (Direction.Y) * distance * (keyFore || keyBack ? sqrt2 : 1.0f);
+				position.X += sin (Direction.Y) * distance * (keyFore || keyBack ? sqrt2 : 1.0f);
+			}
+			if (keyBack) {
+				position.Z -= cos (Direction.Y) * distance * (keyFore || keyBack ? sqrt2 : 1.0f);
+				position.X += sin (Direction.Y) * distance * (keyFore || keyBack ? sqrt2 : 1.0f);
+			}
+			/*if (keyboardState.IsKeyDown (Keys.W)) {
+				position.Z += 1.0f;
+			}
+			if (keyboardState.IsKeyDown (Keys.S)) {
+				position.Z -= 1.0f;
+			}
+			if (keyboardState.IsKeyDown (Keys.A)) {
+				position.X -= 1.0f;
+			} else {
+				position.X += 1.0f;
+			}*/
             Position.Real = position;
         }
 
