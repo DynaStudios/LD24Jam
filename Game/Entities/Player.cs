71fb0b74dbe1255dfe1b7bf292b2ac4ec8de29f9
Arthur Landel <arthur@dyna-studios.com>
2012-08-25 04:31:44 +0200

    player stuff

---
+++ Game/Entities/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+using DynaStudios.LD24.Scenes;
+
+namespace DynaStudios.LD24.Game.Entities
+{
+    public class Player : ActiveEntity
+    {
+        public int Health { get; set; }
+        public int MaxHealth { get; set; }
+        public int Exp { get; set; }
+
+        private GameScreen screen;
+
+        public Player(GameScreen screen)
+        {
+            this.screen = screen;
+        }
+
+        public override void Update(TimeSpan runTime)
+        {
+            if (screen == null || screen.input == null || screen.input.KeyboardState == null)
+            {
+                return;
+            }
+
+            var keyboardState = screen.input.KeyboardState;
+            var position = Position;
+
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
+                position.Z += 1.0f;
+            }
+            else if (keyboardState.IsKeyDown(Keys.S))
+            {
+                position.Z -= 1.0f;
+            }
+            if (keyboardState.IsKeyDown(Keys.A))
+            {
+                position.X -= 1.0f;
+            }
+            else
+            {
+                position.X += 1.0f;
+            }
+        }
+    }
+}
--- Scenes/GameScreen.cs
+++ Scenes/GameScreen.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.Xna.Framework;
 
+using DynaStudios.UI.Input;
+
+using DynaStudios.LD24.Game.Entities;
+
 namespace DynaStudios.LD24.Scenes
 {
     public class GameScreen : UI.Screens.GameScreen
     {
+        public Player Player;
+        public InputState input;
+
+        public GameScreen()
+        {
+            Player = new Player(this);
+        }
+
         public override void Activate(bool instancePreserved)
         {
             base.Activate(instancePreserved);
@@ -12,11 +24,17 @@ namespace DynaStudios.LD24.Scenes
         public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
         {
             base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+            Player.Update(gameTime.TotalGameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
+        }
+
+        public override void HandleInput(GameTime gameTime, InputState input)
+        {
+            base.HandleInput(gameTime, input);
         }
     }
 }
