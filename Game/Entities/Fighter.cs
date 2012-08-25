5228906b8e00772434aca963950412399215a77e
Arthur Landel <arthur@dyna-studios.com>
2012-08-25 16:08:00 +0200

    more combat methods

--- Game/Entities/Fighter.cs
+++ Game/Entities/Fighter.cs
@@ -7,20 +7,44 @@ using DynaStudios.LD24.Game.Equipment;
 
 namespace DynaStudios.LD24.Game.Entities
 {
+    public delegate void PassingFighterDelegate(Fighter fighter);
+
     public abstract class Fighter : ActiveEntity
     {
+        public event PassingFighterDelegate Died;
         public Stats BaseStats { get; protected set; }
         public Stats Stats { get; protected set; }
-        public Weapen Weapen { get; protected set; }
+        public ReadOnlyCollection<IEquipment> Equipment { get; private set; }
+        
+        protected List<Weapen> weapens { get; set; }
+        protected ReadOnlyCollection<Weapen> Weapens { get; set; }
         protected List<IEquipment> equipment = new List<IEquipment>();
 
-        public ReadOnlyCollection<IEquipment> Equipment
+
+        public void HitTarget(Weapen weapen, Fighter target)
         {
-            get { return new ReadOnlyCollection<IEquipment>(equipment); }
+            target.TakeDemage(weapen.GetAttackForce(Stats));
         }
 
-        public void SetWeapen(Weapen weapen)
+        public void TakeDemage(int deamage)
         {
+            Stats.Health -= deamage / Stats.Defence - Stats.Defence;
+            if (Stats.Health <= 0 && Died != null)
+            {
+                Died(this);
+            }
+        }
+
+        public void AddWeapen(Weapen weapen)
+        {
+            weapens.Add(weapen);
+            AddEqipment(weapen);
+        }
+
+        public void RemoveWeapen(Weapen weapen)
+        {
+            weapens.Remove(weapen);
+            RemoveEqipment(weapen);
         }
 
         public void AddEqipment(IEquipment item)
@@ -42,6 +66,8 @@ namespace DynaStudios.LD24.Game.Entities
             {
                 Stats = item.getModfied(Stats);
             }
+            Equipment = new ReadOnlyCollection<IEquipment>(equipment);
+            Weapens = new ReadOnlyCollection<Weapen>(weapens);
         }
     }
 }
