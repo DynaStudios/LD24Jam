8be8c936dd61d453e020b3ed04fb46570d048b70
Arthur Landel <arthur@dyna-studios.com>
2012-08-25 03:47:26 +0200

    some class hirachy

---
+++ Game/Entities/Entity.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace DynaStudios.LD24.Game.Entities {
+
+    public abstract class Entity
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Direction { get; set; }
+    }
+}
